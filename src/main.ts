5ebd7261395a1b9a50f14c85b933cac388827719
Amir Benassayag <amir12061968@gmail.com>
2023-11-19 14:23:59 +0200

    add https module

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
-
+import { readFileSync } from 'fs';
+import { AgentOptions } from 'https';
+import { INestApplication } from '@nestjs/common';
+import { resolve } from 'path';
 
 async function bootstrap() {
 
-  const app = await NestFactory.create(AppModule);
+  let app: INestApplication<any>;
   const origin = [process.env.FRONTEND_URI];
   if (process.env.NODE_ENV == "local") {
+    app = await NestFactory.create(AppModule);
     app.enableCors({
       origin
     })
   }
 
-  else{
+  else {
+
+    const httpsOptions: AgentOptions = {
+      ca: readFileSync(resolve(process.cwd(), 'src', 'crypto/cert')),
+      key: readFileSync(resolve(process.cwd(), 'src', 'crypto/key'))
+    }
+    app = await NestFactory.create(AppModule, { httpsOptions });
     app.use(helmet());
 
-  
+
     if (process.env.CHECK_URI) {
       origin.push(process.env.CHECK_URI);
     }
-    
-   /*  app.enableCors({
-      origin
-    }) */
+
+    /*  app.enableCors({
+       origin
+     }) */
   }
-  
+
   await app.listen(parseInt(process.env.PORT || "8080"));
 }
 bootstrap();
